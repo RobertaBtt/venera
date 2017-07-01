44790260ec383f46e48c8213e12a70676c86ee41
Roby <roberta.btt@gmail.com>
2017-07-01 19:20:29 +0200

    Loghi carousel

--- title_carousel.php
+++ title_carousel.php
@@ -15,17 +15,17 @@
                 <div class='slider-browsers-w clearfix'>
                     <div class='slider-browser slider-browser-left hidden-phone' data-position-bottom='-8%'>
                         <a href="features/pricing_tables.html">
-                            <img alt="Browser-window-1" src="<?php echo get_template_directory_uri(); ?>/assets/images/browser-window.png" />
+                            <img alt="Browser-window-1" src="<?php echo get_template_directory_uri(); ?>/assets/images/photos/LogoAvaaz.png" />
                             
                         </a>
                         
                     </div>
                     <div class='slider-browser slider-browser-center' data-position-bottom='-9%'>
                         
-                        <img alt="Browser-window-2" src="<?php echo get_template_directory_uri(); ?>/assets/images/browser-window.png" />
+                        <img alt="Browser-window-2" src="<?php echo get_template_directory_uri(); ?>/assets/images/photos/logo-greenpeace.png" />
                     </div>
                     <div class='slider-browser slider-browser-right hidden-phone' data-position-bottom='-8%'>
-                        <img alt="Browser-window-3" src="<?php echo get_template_directory_uri(); ?>/assets/images/browser-window.png" />
+                        <img alt="Browser-window-3" src="<?php echo get_template_directory_uri(); ?>/assets/images/photos/logo-ciwf.png" />
                     </div>
                 </div>
             </div>
